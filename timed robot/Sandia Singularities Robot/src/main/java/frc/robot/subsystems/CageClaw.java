3046b575476ea1a111998ed9fbe3fa66cf96a7ff
Sam Dauk <sdauk@eastmountainhigh.net>
2019-01-30 16:06:57 -0700

    made some changes to CageClaw.java

--- timed robot/Sandia Singularities Robot/src/main/java/frc/robot/subsystems/CageClaw.java
+++ timed robot/Sandia Singularities Robot/src/main/java/frc/robot/subsystems/CageClaw.java
@@ -8,6 +8,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 /**
  * Add your docs here.
@@ -15,10 +18,17 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class CageClaw extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-
+  public static final PWMTalonSRX motor8 = new PWMTalonSRX(7);
+  public static final PWMTalonSRX motor9 = new PWMTalonSRX(8);
+  public static final XboxController logitech2 = new XboxController(1);
+  
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void ClawControl() {
+    
   }
 }
