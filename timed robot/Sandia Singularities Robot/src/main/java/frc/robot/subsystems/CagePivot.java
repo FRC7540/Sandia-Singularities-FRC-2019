74f35860f0770900fa02e6a8e808c8c35c87aaf7
Sam Dauk <sdauk@eastmountainhigh.net>
2019-01-30 16:11:41 -0700

    created CagePivot.java

---
+++ timed robot/Sandia Singularities Robot/src/main/java/frc/robot/subsystems/CagePivot.java
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
+/**
+ * Add your docs here.
+ */
+public class CagePivot extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
