377d769cd8cd38107df496a42eafa4ed123825bd
Lucasboi <lstafford@eastmountainhigh.net>
2019-02-02 11:24:23 -0700

    Added button interfaces to the IO

--- timed robot/Sandia Singularities Robot/src/main/java/frc/robot/IO.java
+++ timed robot/Sandia Singularities Robot/src/main/java/frc/robot/IO.java
@@ -7,8 +7,34 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 /**
  * Add your docs here.
  */
 public class IO {
+    private static final int LEFT_HORIZ_AXIS = 0;
+	private static final int LEFT_VERT_AXIS = 1;
+	private static final int RIGHT_HORIZ_AXIS = 4;
+	private static final int RIGHT_VERT_AXIS = 5;
+	private static final int LEFT_Z_AXIS = 3;
+	private static final int RIGHT_Z_AXIS = 2;
+
+	private static final double STICK_DEADZONE = 0.3;
+	private static final double STICK_MAX = 0.97;
+
+	// driver controller setup
+	private Joystick driverController = new Joystick(0);
+	private Button driverButtonA = new JoystickButton(driverController, 1);
+	private Button driverButtonB = new JoystickButton(driverController, 2);
+	private Button driverButtonX = new JoystickButton(driverController, 3);
+	private Button driverButtonY = new JoystickButton(driverController, 4);
+	private Button driverButtonLeftBumper = new JoystickButton(driverController, 5);
+	private Button driverButtonRightBumper = new JoystickButton(driverController, 6);
+	private Button driverButtonBack = new JoystickButton(driverController, 7);
+	private Button driverButtonStart = new JoystickButton(driverController, 8);
+	private Button driverButtonLeftAxisPress = new JoystickButton(driverController, 9);
+    private Button driverButtonRightAxisPress = new JoystickButton(driverController, 10);
 }
