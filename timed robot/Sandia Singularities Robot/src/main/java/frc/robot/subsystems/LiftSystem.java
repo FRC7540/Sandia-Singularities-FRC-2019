41e7ea751bc666b8e441b4435d18a84cf8960d53
Lucasboi <lstafford@eastmountainhigh.net>
2019-02-02 12:49:55 -0700

    Added more changes

--- timed robot/Sandia Singularities Robot/src/main/java/frc/robot/subsystems/LiftSystem.java
+++ timed robot/Sandia Singularities Robot/src/main/java/frc/robot/subsystems/LiftSystem.java
@@ -31,6 +31,24 @@ public class LiftSystem extends Subsystem {
   PWM liftmotor = new PWM(1);
   double currentLvl = 0;
 
+  public void liftStop() {
+    // Stops motor when finished
+  }
+
+  public double getCurrentPosition() {
+    // get the saved current position
+    return 0;
+  }
+
+  public void setCurrentPosition(double desiredPosition) {
+    // make sure that position is valid
+    // save it
+  }
+
+  public void moveTowardsPosition() {
+    // set the motor to move towards the current position
+  }
+
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
@@ -48,29 +66,29 @@ public class LiftSystem extends Subsystem {
     return diff;
   }
 
-  public double desiredPos(double des) {
-    /* Convert rotations to distance
-    double tickDistance = 0;
-    encoder1.setDistancePerPulse(tickDistance);
-    encoder2.setDistancePerPulse(tickDistance);
-    double distance = encoder1.getDistancePerPulse();
-    double level1Dist = 0;
-    double level2Dist = 10*distance;
-    double level3Dist = 20*distance;
-    double level4Dist = 30*distance;
-    */
-    // Get desired distance from buttonz
-    if (logitech2.getAButton() == true)
-       return 0;
-    else if (logitech2.getBButton() == true)
-      return 10;
-    else if (logitech2.getXButton() == true)
-      return 20;
-    else if (logitech2.getYButton() == true)
-      return 30;
-    else
-      return currentLvl;
-  }
+  // public double desiredPos(double des) {
+  //   /* Convert rotations to distance
+  //   double tickDistance = 0;
+  //   encoder1.setDistancePerPulse(tickDistance);
+  //   encoder2.setDistancePerPulse(tickDistance);
+  //   double distance = encoder1.getDistancePerPulse();
+  //   double level1Dist = 0;
+  //   double level2Dist = 10*distance;
+  //   double level3Dist = 20*distance;
+  //   double level4Dist = 30*distance;
+  //   */
+  //   // Get desired distance from buttonz
+  //   if (logitech2.getAButton() == true)
+  //      return 0;
+  //   else if (logitech2.getBButton() == true)
+  //     return 10;
+  //   else if (logitech2.getXButton() == true)
+  //     return 20;
+  //   else if (logitech2.getYButton() == true)
+  //     return 30;
+  //   else
+  //     return currentLvl;
+  // }
 
   public double getCurrentLvl() {
     return currentLvl;
