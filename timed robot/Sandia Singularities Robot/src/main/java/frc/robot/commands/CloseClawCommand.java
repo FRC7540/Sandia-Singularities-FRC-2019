8ee585ea986aae9004834dcd67a4abc2f290aeee
Lucas Stafford <lstafford@eastmountainhigh.net>
2019-02-12 20:47:28 -0700

    Renamed claw interface

--- timed robot/Sandia Singularities Robot/src/main/java/frc/robot/commands/CloseClawCommand.java
+++ timed robot/Sandia Singularities Robot/src/main/java/frc/robot/commands/CloseClawCommand.java
@@ -25,7 +25,7 @@ public class CloseClawCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.clawSubsystem.CloseClaw();
+    Robot.clawSubsystem.closeClaw();
   }
 
   // Make this return true when this Command no longer needs to run execute()
