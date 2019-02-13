e106f3ccd92e21f7bb35db9b071a61675903a48e
Ironispain <jbertrand@eastmountainhigh.net>
2019-02-12 19:27:11 -0700

    fixed everything, probably

--- timed robot/Sandia Singularities Robot/src/main/java/frc/robot/subsystems/Thirdeye.java
+++ timed robot/Sandia Singularities Robot/src/main/java/frc/robot/subsystems/Thirdeye.java
@@ -8,21 +8,23 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.Ultrasonic;
+import frc.robot.RobotMap;
+import frc.robot.commands.DriveCommand;
 
 //adding motors and the ultrasonic sensor
 public class Thirdeye extends Subsystem {
   Ultrasonic thirdeye = new Ultrasonic(1,1);
-  private static final XboxController logitech1 = new XboxController(0);
-  private static final PWMVictorSPX motor1 = new PWMVictorSPX(0);
-  private static final PWMVictorSPX motor2 = new PWMVictorSPX(1);
-  private static final PWMVictorSPX motor3 = new PWMVictorSPX(2);
-  private static final PWMVictorSPX motor4 = new PWMVictorSPX(3);
+  private static final XboxController logitech1 = new XboxController(RobotMap.controller1);
+  private static final PWMVictorSPX motor1 = new PWMVictorSPX(RobotMap.leftMotors[0]);
+  private static final PWMVictorSPX motor2 = new PWMVictorSPX(RobotMap.leftMotors[1]);
+  private static final PWMVictorSPX motor3 = new PWMVictorSPX(RobotMap.rightMotors[0]);
+  private static final PWMVictorSPX motor4 = new PWMVictorSPX(RobotMap.rightMotors[1]);
   SpeedControllerGroup leftmotors = new SpeedControllerGroup(motor1, motor2);
   SpeedControllerGroup rightmotors = new SpeedControllerGroup(motor3, motor4);
   DifferentialDrive maindrive = new DifferentialDrive(leftmotors, rightmotors);
@@ -40,8 +42,17 @@ public class Thirdeye extends Subsystem {
   public void ultrasonicSample() {
     double range = thirdeye.getRangeInches();
     double analogLY = logitech1.getY(Hand.kLeft);
-    if (range == 5)
-     double maindrive = analogLY / -range;
-      maindrive.arcadeDrive(forwardDrive, analogLX);
+    double convert = range * -1;
+    double brakes;
+    if (range <= 5)
+      brakes = analogLY / convert;
+      double forwardDrive = brakes;
+    maindrive.arcadeDrive(forwardDrive, analogLX);
   }
+
+  public void driveSystem() {
+    double analogLY = logitech1.getY(Hand.kLeft);
+    double analogLX = logitech1.getX(Hand.kLeft);
+    double forwardDrive = analogLY * -1;
+    maindrive.arcadeDrive(forwardDrive, analogLX);
 }  
